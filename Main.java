25ffe5dee3e69f8dc14773e38aabad01ecf4f205
m-steph <86425576+m-steph@users.noreply.github.com>
2021-10-12 13:55:25 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-
-
+    int[] houses = new int[]{0,2,3,6,4};
+    House h = modelStreet(houses);
+    System.out.println("next candy value should be 2, it is:"+h.nextHouse.candy);
+    System.out.println("get candy value should be 5, it is:"+getCandy(h,2));
+    System.out.println("the candy value of the house returned should be 3, it is:"+getMaxCandyOnStreet(h,2).candy);
 
   }
   public static House modelStreet(int[] input){
@@ -9,11 +12,11 @@ public class Main {
   }
 
   public static int getCandy(House h, int n){
-    return 0;
+    return 50;
   }
 
   public static House getMaxCandyOnStreet(House h, int n){
-
+    return new House(0);
   }
 
 }
